837ecf575ecf23b49764b0251c11887928725de2
Wonjun Roh <rohw@oregonstate.edu>
2021-04-17 05:17:09 +0900

    Finished calculator.py

--- calculator.py
+++ calculator.py
@@ -10,3 +10,9 @@ def calc_function(x, y):
 
     div = x/y
     print("Division is ", div)
+
+
+x = int(input("Please enter a number for 'x': "))
+y = int(input("Please enter a number for 'y': "))
+
+calc_function(x, y)
