6519fc9521830a77d11ea530558828469603b951
Wonjun Roh <rohw@oregonstate.edu>
2021-04-17 07:34:46 +0900

    Edited divisor function and finished divisor.py

--- divisor.py
+++ divisor.py
@@ -1,7 +1,12 @@
 def divisor(x):
-    print("The divisors are: ")
+    print("The divisors are:", end=" ")
     i = 1
     while i <= x:
         if (x % i == 0):
-            print(i)
+            print(i, end=" ")
         i += 1
+
+
+x = int(input("Please enter a number for its divisors: "))
+
+divisor(x)
