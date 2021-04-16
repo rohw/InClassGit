08558eb66b1dd814b8c96576b79a2f686c00a9b5
Wonjun Roh <rohw@oregonstate.edu>
2021-04-17 07:59:05 +0900

    Edited gen_password function and finished password.py

--- password.py
+++ password.py
@@ -3,5 +3,12 @@ import random
 
 
 def gen_password(length):
+    charactor = string.ascii_letters + string.digits
+    print("Generated password: ", end=")
     for i in range(length):
-        print(random.randint(32, 126), end="")
+        print(random.choice(charactor), end="")
+
+
+x = int(input("Enter number of characters that you want your password to be: "))
+
+gen_password(x)
